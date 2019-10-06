1f3faf2bbab7167d6191671657c563ea0e232530
Walid Haidari <walidhaidari@1x-193-157-237-148.uio.no>
2019-10-06 18:07:33 +0200

    Working on the self ininitialize.

--- Card.java
+++ Card.java
@@ -2,14 +2,19 @@ import java.util.*;
 
 public class Card{
   String suit;
+  //Using specCard for cards Jack up to Spade so I can print them out as for instance CJ DQ AA etc
+  String specCard;
   int val;
+  boolean taken = false;
 
 
   public Card(String suit, int val){
     this.suit = suit;
     this.val = val;
+
   }
 
+  
   public static void main(String[] args) {
     Card c1 = new Card("C", 2);
 
--- Deck.java
+++ Deck.java
@@ -1,10 +1,34 @@
 import java.util.*;
+import java.lang.Math;
 
 public class Deck{
   Card[] cardStack;
+  int deck_size;
 
   public Deck(){
 
+  }
+
+  void self_initialize_deck(){
+    cardStack = new Card[52];
+    deck_size = cardStack.length;
+
+    for (int i = 0;i < deck_size ;i++ ) {
+      for (int j = 1;j<=13 ;j++ ) {
+        if (i == 0) {
+          if (j<10) {
+            
+          }
+        } else if (i == 1) {
+
+        } else if (i == 2) {
+
+        }
+        else if (i == 3) {
+
+        }
+      }
+    }
   }
 
   void draw_card(){
